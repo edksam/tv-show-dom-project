99bbbccb3505f9b100b4e2674d54a13debf80a8c
edksam <paakofisam.sam@gmail.com>
2020-08-31 19:57:17 +0100

    updated level 200 completed, added some css

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //You can edit ALL of the code here
 const search = document.getElementById("search");
 const results = document.getElementById("results");
+const selected = document.getElementById("selected");
 
 let term = "";
 
@@ -31,43 +32,23 @@ const makePageForEpisodes = async () => {
        `;
     });
 
-   function zeroPadded(number) {
+  function zeroPadded(number) {
     return number.toString().padStart(2, "0");
+  }
+  const selectSearch = allEpisodes.filter(
+    (episode) =>
+      episode.name.toLowerCase().includes(term.toLowerCase()) ||
+      episode.summary.toLowerCase().includes(term.toLowerCase())
+  );
+  if (term === "") {
+    selected.innerHTML = "";
+  } else {
+    selected.innerHTML = `${selectSearch.length} out of ${allEpisodes.length} episode(s) selected.`;
   }
 };
 
 window.onload = setup;
-
 search.addEventListener("input", (e) => {
   term = e.target.value;
   makePageForEpisodes(term);
 });
-
-
-// function setup() {
-//   const allEpisodes = getAllEpisodes();
-//   makePageForEpisodes(allEpisodes);
-//  }
-  
-//  function makePageForEpisodes(episodeList) {
-//   const container = document.getElementById("container");
-//   const rootElem = document.getElementById("root");
-//   episodeList.forEach((episode) => {
-//     container.innerHTML += `<div>
-//     <h2>${episode.name} - S${zeroPadded(episode.season)}E${zeroPadded(
-//       episode.number
-//     )}</h2>
-//     <img src="${episode.image.medium}" >
-//     ${episode.summary}
-//         </div>`;
-//   });
-  
-//   function zeroPadded(number) {
-//     return number.toString().padStart(2, "0");
-//   }
-  
-//   console.log(zeroPadded(2));
-//  }
-  
-//  window.onload = setup;
- 
